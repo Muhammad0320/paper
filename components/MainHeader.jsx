7d42f309c4d0a91f80bd4433a2517226370aaedc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-04 17:40:00 +0100

    chore: complete challege

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -4,7 +4,7 @@ function MainHeader() {
   return (
     <div>
       <Link href="/news"> this is the nees oage </Link>
-      <Link href="/news"> this is the news shit page </Link>
+      <Link href="/news/shit"> this is the news shit page </Link>
     </div>
   );
 }
