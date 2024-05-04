f87a34a06264e4c0e8c3c85779c7104a7584343e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-04 17:35:11 +0100

    chore: update page js

---
+++ app/news/[newsDetails]/page.jsx
@@ -0,0 +1,7 @@
+function NewsDetailsPage({ params }) {
+  return (
+    <div>This is the news details page of params {params.newsDetails}</div>
+  );
+}
+
+export default NewsDetailsPage;
---
+++ app/news/page.jsx
@@ -0,0 +1,5 @@
+function NewsPage() {
+  return <div>This is ther fucking news page</div>;
+}
+
+export default NewsPage;
