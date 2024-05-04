7815d55d9032f4c5701fc27f7e7022ea59312be5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-04 19:41:24 +0100

    chore: update default ts

--- app/archive/@latest/default.jsx
+++ app/archive/@latest/default.jsx
@@ -1,7 +1,14 @@
+import NewsList from "@/components/NewsList";
+import { getLatestNews } from "@/lib/news";
+
 function LatestPage() {
+  const latest = getLatestNews();
+
   return (
     <div>
-      <h1> This is the latest parallel page </h1>
+      <h1> Latest News </h1>
+
+      <NewsList data={latest} />
     </div>
   );
 }
--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,21 +1,11 @@
+import NewsList from "@/components/NewsList";
 import { DUMMY_NEWS } from "@/dummyNews";
-import Image from "next/image";
-import Link from "next/link";
 
 function NewsPage() {
   return (
     <>
       <h1> The news page </h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((item) => (
-          <li key={item.id}>
-            <Link href={`/news/${item.slug}`}>
-              <img src={`/images/news/${item.image}`} alt={item.title} />
-              <span> {item.title} </span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList data={DUMMY_NEWS} />
     </>
   );
 }
