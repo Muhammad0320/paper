b19c3428935a306253d09e6d462bc83cc70e7463
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-04 18:30:13 +0100

    chore:  add parallel route

---
+++ app/archive/@latest/page.jsx
@@ -0,0 +1,9 @@
+function LatestPage() {
+  return (
+    <div>
+      <h1> This is the latest parallel page </h1>
+    </div>
+  );
+}
+
+export default LatestPage;
---
+++ app/archive/layout.jsx
@@ -0,0 +1,11 @@
+function ArchiveLayout({ archive, latest }) {
+  return (
+    <div>
+      <h1> News archive </h1>
+      <section id="archive-fiter"> {archive} </section>
+      <section id="archive-latest"> {latest} </section>
+    </div>
+  );
+}
+
+export default ArchiveLayout;
