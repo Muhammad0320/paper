7f15cbd0dc80b033c480000641ab415810749da8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-04 18:24:55 +0100

    chore: update page

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div>
+        <div id="page">
           <MainHeader />
         </div>
         {children}
