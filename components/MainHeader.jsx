69b8f5985d7290eb22c39efd4662f2587b29e4ef
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-04 17:37:09 +0100

    chore:  add main header comp

---
+++ components/MainHeader.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function MainHeader() {
+  return (
+    <div>
+      <Link href="/news"> this is the nees oage </Link>
+      <Link href="/news"> this is the news shit page </Link>
+    </div>
+  );
+}
+
+export default MainHeader;
