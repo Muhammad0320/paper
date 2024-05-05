94886c4194251bc73e9f4f1e4144e2325cf94fac
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-05 12:58:46 +0100

    chore:  add intercepting route

---
+++ app/news/[id]/(.)image/page.jsx
@@ -0,0 +1,20 @@
+import { DUMMY_NEWS } from "@/dummyNews";
+
+function ImageView({ params }) {
+  const newsData = DUMMY_NEWS.find((news) => news.slug === params.id);
+
+  if (!newsData) {
+    notFound();
+  }
+
+  return (
+    <>
+      <h2>This is intercepted</h2>
+      <div className="fullscreen-image">
+        <img src={newsData.image} alt={newsData.title} />
+      </div>
+    </>
+  );
+}
+
+export default ImageView;
